d742e6801558db377038e62f6e3af9ca1969aaef
KeyRoll <kaziu11511@gmail.com>
2019-06-04 11:03:15 +0200

    base configurations

--- MyPhoto/settings.py
+++ MyPhoto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #own
+    'Photo',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR,'assetss')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'assets'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
