d6ae32d33cbd15b77e4da04bd5bc6686c52dff92
KeyRoll <kaziu11511@gmail.com>
2019-06-04 13:40:38 +0200

    fix models

--- Photo/admin.py
+++ Photo/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(watching)
-admin.site.register(followed)
+admin.site.register(Watching)
+admin.site.register(Followed)
+admin.site.register(Post)
